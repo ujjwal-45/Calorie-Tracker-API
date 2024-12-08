2b8f39c7c9309d02a6ee19aee35147117020aaa6
Ujjwal <ujjwalchoudhary065@gmail.com>
2024-12-08 17:23:36 +0530

    updated requests with http status code responses

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,7 +15,7 @@ def get_session():
     finally:
         session.close()
 
-@app.post('/create')
+@app.post('/create', status_code=status.HTTP_201_CREATED)
 def create_post(req: schemas.CreateCalorieEvent, db: Session = Depends(get_session)):
     new_calorie_event = models.CalorieEvent(date=req.date, time=req.time, text=req.text, calories=req.noOfCalories)
     db.add(new_calorie_event)
@@ -31,6 +31,9 @@ def get_all(db: Session = Depends(get_session)):
     return calorie_events
 
 @app.get('/create/{id}')
-def show(id, db: Session = Depends(get_session)):
+def show(id,response:Response, db: Session = Depends(get_session)):
     calorie_event = db.query(models.CalorieEvent).filter(models.CalorieEvent.id == id).first()
+    if not calorie_event:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail" : f"This calorie with id:{id} is not there."}
     return calorie_event
