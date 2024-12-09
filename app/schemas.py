2c2abee310b111e33f346bf8006eeb5dc5ca6887
Ujjwal <ujjwalchoudhary065@gmail.com>
2024-12-09 09:07:49 +0530

    updated schemas with User schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from datetime import time,date
 
 class CreateCalorieEvent(BaseModel):
@@ -6,3 +6,9 @@ class CreateCalorieEvent(BaseModel):
     time:time
     text:str
     noOfCalories:int
+
+class User(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    role: str
