1e68f64245025df6763801b1fd9eb711d23ead5e
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-01-15 20:00:03 +0530

    updated calorie model

--- app/models.py
+++ app/models.py
@@ -2,18 +2,25 @@ from sqlalchemy import Column,String,Integer,Float,Date,Time,Boolean,ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
 
+
+class CalorieEvent(Base):
+    __tablename__ = 'calorie_events'
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date)
+    time = Column(Time)
+    text = Column(String)
+    calories = Column(Float)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship("User", back_populates="calorie_events")
+
+
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     password = Column(String)
     email = Column(String)
     role = Column(String)
 
-class CalorieEvent(Base):
-    __tablename__ = 'calorie_events'
-    id = Column(Integer, primary_key=True)
-    date = Column(Date)
-    time = Column(Time)
-    text = Column(String)
-    calories = Column(Float)
+    calorie_events = relationship("CalorieEvent", back_populates="owner")
